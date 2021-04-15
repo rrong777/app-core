40fa83fcbf37af2dbb8587eedc6477662bc8d45f
wuqr <wql789@qq.com>
2021-04-15 18:53:47 +0800

    表单登录单点登录

---
+++ src/main/java/com/slzh/config/server/SsoServerAuthorizationConfig.java
@@ -0,0 +1,77 @@
+package com.slzh.config.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer // 这个注解之后 这个项目就是一个标准的Oauth2认证服务器了。
+public class SsoServerAuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    // 配置应用服务器（认证服务器给哪些应用发令牌）
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("rrong1")
+                .secret("$2a$10$J57aLwc.CY/r8H8a8Bf8../m0VDWMr5Vr6qnDtIuo9yV/a.JAcRX6")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .redirectUris("http://127.0.0.1:8799/client1/login")
+                .redirectUris("http://localhost:8799/client1/login")
+                .redirectUris("http://example.com")
+                .scopes("all")
+                .and()
+                .withClient("rrong2")
+                .secret("$2a$10$isY1PzfSOSmhEAfJV6QzuOgDUXJ8gV/9UyH/IvK8df/RpnQpvy3yO")
+                .redirectUris("http://localhost:8060/client2/login")
+                .redirectUris("http://127.0.0.1:8060/client2/login")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .redirectUris("http://example.com")
+                .scopes("all");
+    }
+
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("rrongSecret1"));
+        System.out.println(passwordEncoder.encode("rrongSecret2"));
+    }
+    // 配置生成令牌使用jwt的
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(jwtTokenStore()).accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    // 认证服务器的安全配置
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // isAuthenticated() 是SpringSecurity的一个授权表达式。下面这个配置是，要访问认证服务器的tokenKey的时候要经过身份认证。tokenKey
+        // accessTokenConverter.setSigningKey("rrong"); 就是rrong
+        // 发出去的是jwt，当应用拿到jwt的时候，需要解析里面的东西，需要去验证签名，需要验签就需要知道tokenKey，
+        // 应用服务器可以到认证服务器拿这个密钥。你过来拿肯定要经过身份认证才能来拿。
+        // tokenKeyAccess = "denyAll()"  默认是拒绝所有访问
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+
+    // 配置jwtTokenStore 仓库 生成jwt令牌
+    @Bean
+    public TokenStore jwtTokenStore() {
+        // jwtTokenStore只管token的存取
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        // 设置签名使用的密钥
+        accessTokenConverter.setSigningKey("rrong");
+        return accessTokenConverter;
+    }
+}
